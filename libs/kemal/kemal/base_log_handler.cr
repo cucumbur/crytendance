4255bf017029b634641410d15260be5f1cfe0e19
Rick Sheahan <rssheah2@illinois.edu>
2016-11-02 22:57:13 -0500

    gitignore

--- libs/kemal/kemal/base_log_handler.cr
+++
@@ -1,7 +0,0 @@
-module Kemal
-  # All loggers must inherit from `Kemal::BaseLogHandler`.
-  abstract class Kemal::BaseLogHandler < HTTP::Handler
-    abstract def call(context)
-    abstract def write(message)
-  end
-end
